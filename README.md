a3cbe4851db180fb0a74a5d74afcc0620e5ec117
Paolo Tremadio <hello@paolotremadio.com>
2018-01-26 17:05:51 +0000

    Re-written code, made it simpler and streamlined

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# homebridge-minimal-http-blinds
+# homebridge-minimal-http-blinds (forked)
 
 ### What is it?
 
@@ -7,20 +7,12 @@
 The features:
 - You can control your own blinds/roller shutters apparatus with three minimalistic HTTP requests.
 - The control is not a simple binary open/close: **it support percentages**. You can open your blinds at 50% or 65% for instance.
-- Your blinds can still be manually operated. As long at the `get_current_position_url` returns the right value, this plugin will update iOS Home app in real time.
-
-### Who is it for?
-
-Anyone who, just like me, don't know much about homebridge
-but still want a straightforward way to communicate with your own home-made Raspberry Pi blinds/roller shutters controller.
+- Your blinds can still be manually operated. As long at the `get_current_position_url` returns the right value, this plugin will update iOS Home app periodically.
 
 ### How to use it
 
 #### 1] Install it into your homebridge instance
 
-The installation instructions differs depending on how you installed homebridge.
-
-Usually, it's something like _"add this to your homebridge's `install.sh`"_
 ````bash
 npm install -g homebridge-minimal-http-blinds
 ````
@@ -31,37 +23,18 @@ Here is an homebridge's `config.json` with the minimal valid configuration:
 
 ````json
 {
-    "bridge": {
-        "name": "DemoMinimalisticHttpBlinds",
-        "username": "AA:BB:CC:DD:EE:FF",
-        "port": 51826,
-        "pin": "123-45-678"
-    },
-  
-    "description": "DEV NODEJS MACBOOK",
-  
     "accessories": [
         {
             "name": "Kitchen Blinds",
             "accessory": "MinimalisticHttpBlinds",
   
             "get_current_position_url": "http://192.168.1.55/get/current_position/",
-            "set_target_position_url": "http://192.168.1.55/set/%position%",
-            "get_current_state_url": "http://192.168.1.55/get/current_state/"
+            "set_target_position_url": "http://192.168.1.55/set/%position%"
         }
   
-    ],
-  
-    "platforms": []
+    ]
 }
 ````
-
-Beware, I'm a lazy ass!  
-These three parameters are not checked!  
-(`get_current_position_url`, `set_target_position_url`, `get_current_state_url`)  
-If you forgot to write them in your accessory, the module will crash.
-
-Also, in the `set_target_position_url` parameter, the placeholder `%position%` will be replaced by the value selected in the iPhone's Home App. 
 
 #### 3] More configuration
 
@@ -73,14 +46,8 @@ Here are them all with their default values.
 {
     "get_current_position_method": "GET",
     "set_target_position_method": "POST",
-    "get_current_state_method": "GET",
-    
-    "get_current_position_expected_response_code": "200",
-    "set_target_position_expected_response_code": "204",
-    "get_current_state_expected_response_code": "200",
     
     "get_current_position_polling_millis": "500",
-    "get_current_state_polling_millis": "500"
 }
 ````
 
@@ -95,28 +62,5 @@ This URL must return the current blinds position **in plaintext**, from 0 to 100
 
 ##### 4.2] `set_target_position_url`
 
-This URL must trigger the blinds movement.  
-(That's the part you've done with your Raspberry Pi)  
-The requested opening position is, once again, an integer from 0 to 100.
-Please note that is passed **directly in the URL**. (It's the `%position%` placeholder)  
-
-Yep, that's it.  
-Not a single trace of json.  
-Are we barbarians or are we not?    
-
-
-##### 4.3] `get_current_state_url`
-
-This is the trickiest URL.  
-It must return one of these three integers **in plaintext**:
-- 0 if the blinds are closing
-- 1 if the blinds are opening
-- 2 if the blinds are idling
-
-Once again, that's your part of the job, the one you're supposed to implement in your blind manager thingymagic.
-
-________________________________________
-
-# That's all
-
-## Enjoy
+This URL must trigger the blinds movement. The requested opening position is, once again, an integer from 0 to 100 (0 being closed and 100 opened).
+Please note that the target position is passed **directly in the URL**. (It's the `%position%` placeholder)  
--- index.js
+++ index.js
@@ -1,191 +1,143 @@
-var request = require('request');
-var Service, Characteristic;
+const request = require('request');
+let Service, Characteristic;
 
-module.exports = function(homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory('homebridge-minimal-http-blinds', 'MinimalisticHttpBlinds', MinimalisticHttpBlinds);
-    console.log('Loading MinimalisticHttpBlinds accessories...');
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory('homebridge-minimal-http-blinds', 'MinimalisticHttpBlinds', MinimalisticHttpBlinds);
 };
 
-function MinimalisticHttpBlinds(log, config) {
+/**
+ * About WindowCover position: 100 is fully open, 0 is fully closed
+ */
+
+class MinimalisticHttpBlinds {
+  constructor(log, config) {
     this.log = log;
 
     // Required parameters
-    this.get_current_position_url = config.get_current_position_url;
-    this.set_target_position_url  = config.set_target_position_url;
-    this.get_current_state_url    = config.get_current_state_url;
+    this.getCurrentPositionUrl = config.get_current_position_url || "";
+    this.setTargetPositionUrl = config.set_target_position_url || "";
 
     // Optional parameters: HTTP methods
-    this.get_current_position_method = config.get_current_position_method || 'GET';
-    this.set_target_position_method  = config.set_target_position_method  || 'POST';
-    this.get_current_state_method    = config.get_current_state_method    || 'GET';
-
-    // Optional parameters: expected HTTP response codes
-    this.get_current_position_expected_response_code = parseInt(config.get_current_position_expected_response_code) || 200;
-    this.set_target_position_expected_response_code  = parseInt(config.set_target_position_expected_response_code)  || 204;
-    this.get_current_state_expected_response_code    = parseInt(config.get_current_state_expected_response_code)    || 200;
+    this.getCurrentPositionMethod = config.get_current_position_method || 'GET';
+    this.setTargetPositionMethod = config.set_target_position_method || 'POST';
 
     // Optional parameters: polling times
-    this.get_current_position_polling_millis = parseInt(config.get_current_position_polling_millis) || 500;
-    this.get_current_state_polling_millis    = parseInt(config.get_current_state_polling_millis)    || 500;
+    this.currentPositionPollingInterval = parseInt(config.get_current_position_polling_millis) || 500;
 
     // Internal fields
-    this.current_position = undefined;
-    this.current_state = undefined;
+    this.lastKnownPosition = null;
+    this.currentPositionTimer = null;
 
-    this.get_current_position_callbacks = [];
-    this.get_target_position_callbacks = [];
-    this.get_current_state_callbacks = [];
+    this.targetPosition = 100;
 
-    // Initializing things
-    this.start_current_position_polling();
-    this.start_current_state_polling();
-    this.init_service();
-}
+    this.windowCoveringService = new Service.WindowCovering(this.name);
 
-MinimalisticHttpBlinds.prototype.init_service = function() {
-    this.service = new Service.WindowCovering(this.name);
+    this.windowCoveringService
+      .getCharacteristic(Characteristic.CurrentPosition)
+      .on('get', this.getCurrentPosition.bind(this));
 
-    this.service.getCharacteristic(Characteristic.CurrentPosition).on('get', function(callback) {
-        this.get_current_position_callbacks.push(callback);
-    }.bind(this));
+    this.windowCoveringService
+      .getCharacteristic(Characteristic.TargetPosition)
+      .on('get', this.getTargetPosition.bind(this))
+      .on('set', this.setTargetPosition.bind(this));
 
-    this.service.getCharacteristic(Characteristic.TargetPosition).on('get', function(callback) {
-        this.get_target_position_callbacks.push(callback);
-    }.bind(this));
-    this.service.getCharacteristic(Characteristic.TargetPosition).on('set', this.set_target_position.bind(this));
+    // Initialise accessories, update both CurrentPosition and TargetPosition
+    this.currentPositionTimerAction(true);
+    this.log(`Polling blind state every ${this.currentPositionPollingInterval}ms`);
+  }
 
-    // Note: iOS's Home App subtracts CurrentPosition from TargetPosition to determine if it's opening, closing or idle.
-    // It absolutely doesn't care about Characteristic.PositionState, which is supposed to be :
-    // PositionState.INCREASING = 1, PositionState.DECREASING = 0 or PositionState.STOPPED = 2
-    // But in any case, let's still implement it
-    this.service.getCharacteristic(Characteristic.PositionState).on('get', function() {
-        this.get_current_state_callbacks.push(callback);
-    }.bind(this));
-};
+  getServices() {
+    return [this.windowCoveringService];
+  }
 
-MinimalisticHttpBlinds.prototype.start_current_position_polling = function() {
-    setTimeout(this.update_current_position.bind(this), this.get_current_position_polling_millis);
-};
+  getCurrentPosition(callback) {
+    callback(null, this.lastKnownPosition);
+  }
 
-MinimalisticHttpBlinds.prototype.start_current_state_polling = function() {
-    setTimeout(this.update_current_state.bind(this), this.get_current_state_polling_millis);
-};
+  startCurrentPositionTimer() {
+    this.stopCurrentPositionTimer();
 
-MinimalisticHttpBlinds.prototype.update_current_position = function() {
-    request({
-        url: this.get_current_position_url,
-        method: this.get_current_position_method,
-        timeout: 5000
-    }, function(error, response, body) {
-        if (error || response.statusCode != this.get_current_position_expected_response_code) {
-            this.log('Error when polling current position: ' + body);
-            this.start_current_position_polling();
-            return;
+    this.currentPositionTimer = setTimeout(
+      this.currentPositionTimerAction.bind(this),
+      this.currentPositionPollingInterval
+    );
+  }
+
+  currentPositionTimerAction(updateTargetPosition) {
+    request(
+      {
+        url: this.getCurrentPositionUrl,
+        method: this.getCurrentPositionMethod,
+        timeout: 15000,
+      },
+      (error, response, body) => {
+        if (error) {
+          this.log(`Error setting the new position: ${body} -- ${error.toString()}`);
+          this.startCurrentPositionTimer();
+          return;
         }
 
-        var new_position = parseInt(body);
+        const position = parseInt(body);
 
-        if (this.get_current_position_callbacks.length > 0) {
-            this.get_current_position_callbacks.forEach(function (callback) {
-                this.log('calling callback with position: ' + new_position);
-                callback(null, new_position);
-            }.bind(this));
-            this.log('Responded to ' + this.get_current_position_callbacks.length + ' CurrentPosition callbacks!');
-            this.get_current_position_callbacks = [];
-        }
-        else if (new_position !== this.current_position && !this.notify_ios_blinds_has_stopped) {
-            this.service.getCharacteristic(Characteristic.CurrentPosition).setValue(new_position);
-            this.log('Updated CurrentPosition to value ' + new_position);
+        this.setLastKnownPosition(position);
+
+        if (updateTargetPosition) {
+          this.windowCoveringService
+            .getCharacteristic(Characteristic.TargetPosition)
+            .setValue(position)
         }
 
-        if (this.notify_ios_blinds_has_stopped) {
-            this.notify_ios_blinds_has_stopped = false;
+        this.startCurrentPositionTimer();
+      }
+    );
+  }
 
-            this.log('Updated CurrentPosition and TargetPosition to value ' + new_position);
-            this.service.getCharacteristic(Characteristic.CurrentPosition).setValue(new_position);
-            this.service.getCharacteristic(Characteristic.TargetPosition).setValue(new_position, null, {
-                'plz_do_not_actually_move_the_blinds': true
-            });
-        }
+  stopCurrentPositionTimer() {
+    if (this.currentPositionTimer) {
+      clearTimeout(this.currentPositionTimer);
+    }
+  }
 
-        this.current_position = new_position;
-        this.start_current_position_polling();
-    }.bind(this));
-};
+  setLastKnownPosition(value) {
+    if (this.lastKnownPosition !== value) {
+      this.log(`Blind has moved, new position: ${value}`);
 
-MinimalisticHttpBlinds.prototype.update_current_state = function() {
-    request({
-        url: this.get_current_state_url,
-        method: this.get_current_state_method,
-        timeout: 5000
-    }, function(error, response, body) {
-        if (error || response.statusCode != this.get_current_state_expected_response_code) {
-            this.log('Error when polling current state: ' + body);
-            this.start_current_state_polling();
-            return;
-        }
-
-        var new_state = parseInt(body);
-
-        if (new_state !== this.current_state && new_state === 2)
-            this.notify_ios_blinds_has_stopped = true;
-
-        if (this.get_current_state_callbacks.length > 0) {
-            this.get_current_state_callbacks.forEach(function (callback) {
-                callback(null, new_state);
-            }.bind(this));
-            this.log('Responded to ' + this.get_current_state_callbacks.length + ' PositionState callbacks!');
-            this.get_current_state_callbacks = [];
-        }
-        else if (new_state !== this.current_state) {
-            // Sooo, yeah... We're updating PositionState, but iOS doesn't care anyway... we still do it for the lolz.
-            this.service.getCharacteristic(Characteristic.PositionState).setValue(new_state);
-            this.log('Updated PositionState to value ' + new_state);
-        }
-
-        // This is ugly: we're faking the target position to either 0, 100 or the current position,
-        // so that iOS's Home App displays the right state (opening, closing, idle)
-        var target_position = this.current_position;
-        if (new_state === 1) target_position = 100;
-        else if (new_state === 0) target_position = 0;
-
-        if (this.get_target_position_callbacks.length > 0) {
-            this.get_target_position_callbacks.forEach(function (callback) {
-                callback(null, target_position);
-            }.bind(this));
-            this.log('Responded to ' + this.get_target_position_callbacks.length + ' TargetPosition callbacks!');
-            this.get_target_position_callbacks = [];
-        }
-
-        this.current_state = new_state;
-        this.start_current_state_polling();
-    }.bind(this));
-};
-
-
-MinimalisticHttpBlinds.prototype.set_target_position = function(position, callback, context) {
-    if (context && context.plz_do_not_actually_move_the_blinds) {
-        this.log('set_target_position is ignoring an actual request...');
-        callback(null, position);
-        return;
+      this.windowCoveringService
+        .getCharacteristic(Characteristic.CurrentPosition)
+        .setValue(value);
     }
 
-    this.log('Setting new target position: ' + position + ' => ' + this.set_target_position_url.replace('%position%', position));
-    request({
-        url: this.set_target_position_url.replace('%position%', position),
-        method: this.set_target_position_method
-    }, function(error, response, body) {
-        if (error || response.statusCode != this.set_target_position_expected_response_code) {
-            this.log('Error when setting new target position: ' + body);
-            return;
-        }
-        this.log('Target position set to ' + position);
-        callback(null)
-    }.bind(this));
-};
+    this.lastKnownPosition = value;
+  }
 
-MinimalisticHttpBlinds.prototype.getServices = function() {
-    return [this.service];
-};
+  getTargetPosition(callback) {
+    callback(null, this.targetPosition);
+  }
+
+  setTargetPosition(position, callback) {
+    this.targetPosition = position;
+
+    this.log(`Requested new position: ${position}`);
+    this.stopCurrentPositionTimer();
+
+    request(
+      {
+        url: this.setTargetPositionUrl.replace('%position%', position),
+        method: this.setTargetPositionMethod,
+        timeout: 15000,
+      },
+      (error, response, body) => {
+        if (error) {
+          this.log(`Error setting the new position: ${body} -- ${error.toString()}`);
+          return;
+        }
+
+        this.setLastKnownPosition(position);
+        this.startCurrentPositionTimer();
+        callback(null);
+      }
+    );
+  }
+}
